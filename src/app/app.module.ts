ea29cacca8f22beb3205c72212048739f9efaa7c
willian <wruizza@cesde.net>
2022-12-21 11:31:06 -0500

    tenemos l tabla y el filter y paginacion funcional

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-//importar la libreria d Prime ng
-import { MatButtonModule } from '@angular/material/button';
-//formaularios reactivos
-import { ReactiveFormsModule } from '@angular/forms';
-//importar las solicitudes http
-import { HttpClientModule } from '@angular/common/http';
-//importar para trabajar componenetes tablas,pagiancion etc
+//Imports Componentes
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MomentDateModule } from '@angular/material-moment-adapter';
-//para trabajar con las alertas
-import { MessagesModule } from 'primeng/messages';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-//para trabajar con los dialogos
-import { MatDialogModule } from '@angular/material/dialog';
-//trabaar con grit
-import { MatGridListModule } from '@angular/material/grid-list';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatRippleModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTreeModule } from '@angular/material/tree';
 
 @NgModule({
   declarations: [AppComponent],
@@ -31,19 +34,28 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    MatPaginatorModule,
     MatTableModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MomentDateModule,
-    MessagesModule,
-    MatSnackBarModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
     MatDialogModule,
+    MatDatepickerModule,
+    MatDividerModule,
+    MatExpansionModule,
     MatGridListModule,
-    MatFormFieldModule
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatTreeModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
